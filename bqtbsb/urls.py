ae6746d92ce6d25045710cc49dae70f30084b7a0
npoudelp <npoudelp@gmail.com>
2023-11-11 18:46:10 +0545

    url path for drive and upload added

--- bqtbsb/urls.py
+++ bqtbsb/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drive import urls as drive_path
+from bqtbsb import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.dashboard, name='dashboard'),
+    path('drive/', include(drive_path), name='drive'),
 ]
