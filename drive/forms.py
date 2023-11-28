fb424b5e3fd5d04f0a2be9f6dba90037e5685efa
npoudelp <npoudelp@gmail.com>
2023-11-28 22:50:37 +0545

    tags form added

--- drive/forms.py
+++ drive/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from drive.models import upload_files
+from drive.models import upload_files, tags
 
 class upload_files_form(forms.ModelForm):
     class Meta:
         model = upload_files
-        fields = ['file_path', 'file_status']
+        fields = ['file_path', 'file_status', 'image_tag']
+
+
+class tags_form(forms.ModelForm):
+    class Meta:
+        model = tags
+        fields = ['tag_name']
