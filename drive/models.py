5af15c49c7e29345e98d9403fb9cbe84d093a0dd
npoudelp <npoudelp@gmail.com>
2023-11-28 22:50:51 +0545

    tags model created

--- drive/models.py
+++ drive/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 
 # Create your models here.
 
+class tags(models.Model):
+    tag_name = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.tag_name
+
+
 class upload_files(models.Model):
     FILE_STATUS_CODE = [
         ('0', 'Red'),
@@ -13,6 +20,7 @@ class upload_files(models.Model):
     upload_date = models.DateField(default=timezone.now(), blank=True, null=False)
     file_status = models.CharField(choices=FILE_STATUS_CODE, max_length=20, null=False, blank=False, default="Red")
     is_shared = models.BooleanField(default=False)
+    image_tag = models.ForeignKey(tags, on_delete=models.PROTECT, null=False, blank=True, default="Null")
     
     def __str__(self):
         return self.file_path
