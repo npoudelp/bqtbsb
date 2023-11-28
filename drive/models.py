5af4763a4f9862ed41a1ef1167eda685a1797807
npoudelp <npoudelp@gmail.com>
2023-11-28 11:13:53 +0545

    upload file database created

--- drive/models.py
+++ drive/models.py
@@ -4,8 +4,13 @@ from django.utils import timezone
 # Create your models here.
 
 class upload_files(models.Model):
+    FILE_STATUS_CODE = [('Red'), ('Yellow'), ('Green')]
     file_path = models.CharField(max_length=100, null=False, blank=False)
-    upload_date = models.DateField(default=timezone.now())
+    upload_date = models.DateField(default=timezone.now(), blank=True, null=False)
+    file_status = models.CharField(choices=FILE_STATUS_CODE, max_length=20, null=False, blank=False)
+    is_shared = models.BooleanField(default=False)
+    
+
 
     
 
