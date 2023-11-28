2e4b8b5b072e1797d9b4891226fbe0ee9281f36f
npoudelp <npoudelp@gmail.com>
2023-11-28 19:03:49 +0545

    static file base decleared in urlpatterns

--- bqtbsb/urls.py
+++ bqtbsb/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from drive import urls as drive_path
 from bqtbsb import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
     path('drive/', include(drive_path), name='drive'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
