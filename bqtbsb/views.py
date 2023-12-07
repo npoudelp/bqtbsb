40943690fec286f8d504d616c8d7264481969107
npoudelp <npoudelp@gmail.com>
2023-12-07 20:24:51 +0545

    views for login url managed

--- bqtbsb/views.py
+++ bqtbsb/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='login')
 def dashboard(request):
     res = {}
     return render(request, "dashboard.html", res)
+
+
+def login(request):
+    res = {}
+    return render(request, 'login.html', res)
