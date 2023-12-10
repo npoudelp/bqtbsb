252ae43468d7f3b216b85a8b2c82d42d041fa9af
npoudelp <npoudelp@gmail.com>
2023-12-10 09:25:06 +0545

    model for user profile made in main application

---
+++ bqtbsb/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+
+class my_profile(models.Model):
+    user_name = models.CharField(max_length=30, blank=False, null=False)
+    last_update = models.DateField(default=timezone.now(), blank=True, null=False)
+    user_email = models.EmailField(null=False, blank=False)
+    user_image = models.ImageField(null=False, blank=False, upload_to='user_profile_image')
+    user_address = models.CharField(null=False, blank=False, max_length=40)
+    user_phone = models.CharField(null=False, blank=False, max_length=15)
