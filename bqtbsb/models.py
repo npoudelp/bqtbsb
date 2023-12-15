8fd9da0415149446b233ccedb95b418439077b65
npoudelp <npoudelp@gmail.com>
2023-12-15 20:45:40 +0545

    models for user profile made

--- bqtbsb/models.py
+++ bqtbsb/models.py
@@ -9,3 +9,7 @@ class my_profile(models.Model):
     user_image = models.ImageField(null=False, blank=False, upload_to='user_profile_image')
     user_address = models.CharField(null=False, blank=False, max_length=40)
     user_phone = models.CharField(null=False, blank=False, max_length=15)
+    user_user = models.CharField(null=False, blank=True, max_length=50)
+
+    def __str__(self):
+        return self.user_name
