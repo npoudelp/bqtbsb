509fa5930fbd4a0f566cd079bd0116f67d8e23f9
npoudelp <npoudelp@gmail.com>
2023-12-19 21:13:22 +0545

    data for frame/base renderd

--- bqtbsb/views.py
+++ bqtbsb/views.py
@@ -4,11 +4,14 @@ from django.contrib.auth import authenticate, logout, login
 from django.contrib import messages
 from bqtbsb.models import my_profile as profile
 from bqtbsb.forms import my_profile_form
+from django.http import Http404
 
 
 @login_required(login_url='login')
 def dashboard(request):
-    res = {}
+    res = {
+        'profile': profile.objects.get(user_user=request.user.username),
+    }
     return render(request, "dashboard.html", res)
 
 
@@ -41,10 +44,36 @@ def user_logout(request):
 @login_required(login_url='login')
 def my_profile(request):
 
-    profile_data = profile.objects.all()
+    update = False
+    try:
+        data = profile.objects.get(user_user=request.user.username)
+        update_form = my_profile_form(instance=data)
+        update = True
+
+    except profile.DoesNotExist:
+        data = None
+        update_form = my_profile_form
+        redirect('my_profile')
+
+    if request.method == 'POST':
+        update_profile_data = my_profile_form(request.POST, request.FILES, instance=data)
+        add_profile = my_profile_form(request.POST, request.FILES)
+        if update_profile_data.is_valid():
+            if update_profile_data.save():
+                messages.success(request, "Profile updated...")
+            else:
+                messages.error(request, "Error updating profile...")
+        
+        if add_profile.is_valid() and update == False:
+            hold = add_profile.save(commit=False)
+            hold.user_user = request.user.username
+            hold.save()
+
+        else:
+            pass
 
     res = {
-        'profile':profile_data,
-        'profile_update_form': my_profile_form()
+        'profile':data,
+        'profile_update_form': update_form,
     }
     return render(request, 'my_profile.html', res)
